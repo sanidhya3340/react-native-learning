b9c838323d3be39d59464dc31b39b39215456f6c
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2022-06-23 15:57:37 +0530

    Detecting Orientation

--- App.js
+++ App.js
@@ -1,43 +1,22 @@
-// import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
-  Text,
+  SafeAreaView,
   View,
-  Button,
   Platform,
   StatusBar,
-  Alert,
+  Dimensions,
 } from "react-native";
-
 export default function App() {
   // console.log("App executed");
   // const handlePress = () => console.log("text being clicked");
-
+  // console.log(useDimensions());
+  // console.log(Dimensions.get("screen"));
   return (
-    <View style={styles.container}>
-      {/* <Text numberOfLines={1}>
-        Hello React native! This is a reallyh really long test and I am writing
-        this jus t to enjoy my process of learning and development in the world!
-      </Text> */}
-      <Button
-        color="orange"
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("Button Tapped", "My Message", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-
-      {/* <Button
-        color="green"
-        title="second button with prompt"
-        onPress={() =>
-          Alert.prompt("My title", "My message", (text) => console.log(text))
-        }
-      /> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{ backgroundColor: "dodgerblue", width: "100%", height: "30%" }}
+      ></View>
+    </SafeAreaView>
   );
 }
 
@@ -48,11 +27,5 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // justifyContent: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  stretch: {
-    width: 50,
-    height: 200,
-    resizeMode: "stretch",
-    backgroundColor: "dodgerblue",
   },
 });
