e1d9246ae0aea7c8f9c67f2b35ce2b70c9f00cdf
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2022-06-23 15:17:17 +0530

    buttons and alerts

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  Image,
-  TouchableNativeFeedback,
-  View,
-} from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default function App() {
   // console.log("App executed");
@@ -17,14 +11,24 @@ export default function App() {
         Hello React native! This is a reallyh really long test and I am writing
         this jus t to enjoy my process of learning and development in the world!
       </Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
-        <View
-          style={styles.stretch}
-          onLoadEnd={() => console.log("Image Loaded")}
-          source={require("./assets/favicon.png")}
-        />
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("Button Tapped", "My Message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+
+      <Button
+        color="green"
+        title="second button with prompt"
+        onPress={() =>
+          Alert.prompt("My title", "My message", (text) => console.log(text))
+        }
+      />
     </View>
   );
 }
