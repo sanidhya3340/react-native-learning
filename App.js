1242c924e99b79a100add0f56896589da4ca92b8
sanidhya3340 <sanidhyabhardwaj3340@gmail.com>
2022-06-23 14:04:36 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
 
 export default function App() {
+  // console.log("App executed");
+  // const handlePress = () => console.log("text being clicked");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1}>
+        Hello React native! This is a reallyh really long test and I am writing
+        this jus t to enjoy my process of learning and development in the world!
+      </Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
+        <View
+          style={styles.stretch}
+          onLoadEnd={() => console.log("Image Loaded")}
+          source={require("./assets/favicon.png")}
+        />
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  stretch: {
+    width: 50,
+    height: 200,
+    resizeMode: "stretch",
+    backgroundColor: "dodgerblue",
   },
 });
